f89882d75c9fb2d4e95c49a401f2bb579ea3d873
jaz <eijazallibhai@gmail.com>
2024-02-14 22:03:22 +0000

    added setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="keycove",
+    version="0.1.0",
+    description="A package for api key authorization.",
+    packages=find_packages(),
+    install_requires=[],
+)
