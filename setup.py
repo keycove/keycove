8198eb4ef46c1361abeaa6d8c8c7365a93deffc2
jaz <eijazallibhai@gmail.com>
2024-02-16 18:37:33 +0000

    updated version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="keycove",
-    version="0.0.1",
+    version="0.1.0",
     description="An API key authorization library",
     packages=find_packages(),
     install_requires=[],
